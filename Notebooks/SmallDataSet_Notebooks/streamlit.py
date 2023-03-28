b0782d77e8126a0494e44716f1022d52883aadff
Femke <femkelutz@gmail.com>
2023-03-28 15:52:04 +0200

    first try with streamlit

--- Notebooks/SmallDataSet_Notebooks/streamlit.py
+++ Notebooks/SmallDataSet_Notebooks/streamlit.py
@@ -1,12 +1,20 @@
 import streamlit as st
 import pandas as pd
-#background
+import streamlit as st
+from HybridModel_Class import hybrid_model
+from CFKnnMeansModel_Class import collab_filtering_Kmeans_Model
+from CB_TFIDF_CosineSimilarity import tfidf_cosine_sim_model
+import pickle
 
+
+#FUNCTION definition
 def MovieList():
-    df=pd.read_csv("../../Data/ml-latest-small/PreprocessedData_ml_latest_year_small.csv",index_col=0)
+    df=pd.read_csv("MovieRecommendationHybrid/Data/ml-latest-small/PreprocessedData_ml_latest_year_small.csv",index_col=0)
     movies=df["title"].unique()
     return movies
 
+
+#BACKGROUND
 def add_bg_from_url():
     st.markdown(
          f"""
@@ -24,28 +32,46 @@ def add_bg_from_url():
 add_bg_from_url() 
 #end of back ground
 
-st.sidebar.header("Options")
+#TITEL
+# match title color with carpet light and move it a bit 
+st.markdown(
+    """
+    <style>
+    /* CSS to move the title up */
+    .title {
+        margin-top: -100px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-st.title("Movie recommendation system")
-button1 = st.button("click me")
-if button1:
-    st.write("this is some text")
-st.header("Start of checkbox section")
-like = st.checkbox("Do you like this app?")
-button2= st.button("Submit")
-if button2:
-    if like:
-        st.write("thanks. I like it as well")
-    else:
-        st.write("im sorry, you have bad tastes")
-st.header("Movies")
-user_text = st.text_input("What is your favourite movie?")
-if st.button("Save"):
-    st.write(user_text)
+st.markdown("<h1 style=color:rgb(205,107,136)>Movie recommendation system</h1>", unsafe_allow_html=True)
+#### END of Title
 
-#movies = ["Elvid, the movie star", "Athoy, the movie star", "Femke, upcoming movie star"]
+#DROPDOWN. enable only one selection
 movies =MovieList()
-options = st.multiselect(label="Select Movies", options=movies)
+options = st.multiselect(label="Select Movie", options=movies, max_selections=2)
+if options:
+    st.write(f"You selected {len(options)} movie:")
+    for movie in options:
+        st.write(f"- {movie}")
+else:
+    st.write("Please select one movie")
 
-st.text("")
-st.text("")
+### Movierecommendation button###
+def run_calculation():
+    # Call the function from the other notebook
+    knn_model = pickle.load(open('MovieRecommendationHybrid/Notebooks/SmallDataSet_Notebooks/Model_KNN_Means.sav', 'rb'))
+    cos_sim_model = pickle.load(open('MovieRecommendationHybrid/Notebooks/SmallDataSet_Notebooks/Model_tfidf_cosine_sim.sav', 'rb'))
+    HybridModel=hybrid_model(knn_model,cos_sim_model)
+    df=pd.read_csv("MovieRecommendationHybrid/Data/ml-latest-small/PreprocessedData_ml_latest_year_small.csv",index_col=0)
+    content_df,collab_df,hybrid_df=HybridModel.recommend_similar_items_hybrid(options,df,10) #options was "user_input"
+    # Display the result in Streamlit
+    st.write("The result is:", hybrid_df.head(n=10))
+#import pickle
+#hybrid_filename = "MovieRecommendationHybrid/Notebooks/SmallDataSet_Notebooks/Model_hybrid.sav"
+#hybrid_model = pickle.load(open(hybrid_filename, 'rb'))
+# Create a button that calls the run_calculation function when clicked
+if st.button("Go"):
+    run_calculation()
